2f850a560ce36b9a873cdcbaae372b95d129c500
feraistanti <fistanti551@gmail.com>
2025-10-20 10:20:25 +0700

    Update data mahasiswa berdasarkan ID, kirim respon sukses atau error sesuai hasil

--- index.js
+++ index.js
@@ -60,3 +60,18 @@ app.post('/api/users', (req, res) =>{
     )
 });
 
+app.put('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const {nama, nim, kelas} = req.body;
+    db.query(
+        'UPDATE mahasiswa SET nama = ?, nim = ?, kelas = ?, WHERE id = ?',
+        [nama, nim, kelas, userId],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database' });
+            }
+            res.json({ message: 'User update successfully'});
+        }
+    );
+});
